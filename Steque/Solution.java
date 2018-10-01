4c561ad8cf5c48ae147992756058e34331fbc0f4
ThotaVignan <thota.vignan@msitprogram.net>
2018-10-01 15:36:16 +0530

    "check_style_score -> 0 of 2 passed. style score: 0/1 "

--- Steque/Solution.java
+++ Steque/Solution.java
@@ -1,4 +1,90 @@
 import java.util.*;
+class Node
+{
+	int data;
+	Node adress;
+	Node(int val)
+	{
+		data = val;
+		adress = null;
+	}
+}
+class Operations
+{
+	Node head = null;
+	Node tail = null;
+	static int count = 0;
+
+	public void insert_Starting(int val)
+	{
+		count++;
+		Node node = new Node(val);
+		if(head == null)
+		{
+			tail = node;
+		}
+		node.adress=head;
+		head = node;
+		PrintList();
+	}
+	public void insert_ending(int val)
+	{
+		count++;
+		Node node = new Node(val);
+		if(head == null)
+		{
+			head = node;
+			tail = node;
+			PrintList();
+			return;
+		}
+		tail.adress = node;
+		tail = tail.adress;
+		PrintList();
+	}
+	public void delete_starting()
+	{
+		if(count == 0)
+		{
+			System.out.println("Steque is empty.");
+			return;
+		}
+		count--;
+		head = head.adress;
+		PrintList();
+		
+	}
+	public void delete_Ending()
+	{
+		count--;
+		Node temp = head;
+		while(temp.adress.adress != null)
+		{
+			temp = temp.adress;
+		}
+		temp.adress = null;
+	}
+	public void PrintList()
+	{
+		Node temp = head;
+		if(count>2)
+		{
+			while(temp.adress!=null)
+			{
+				System.out.print(temp.data+", ");
+				temp = temp.adress;
+			}
+			System.out.println(temp.data);
+
+		}
+		if(count == 1)
+		{
+			System.out.println(temp.data);
+		}
+		
+	}
+}
+
 class Solution
 {
 	public static void main(String[] args) {
